6374e7183cf62da464545877b558328bf4b003b7
sshakndr <whenbored777@gmail.com>
2023-05-19 16:31:12 +0700

    headers...

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "*"
     );
     res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
